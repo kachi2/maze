942684c663bc1e9844aceff439710b95ec059cb8
Michael Kachi <jesmikky@gmail.com>
2022-12-09 09:27:13 +0100

    fav added

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -224,7 +224,10 @@
                      </div>
                 </div>
                  <div class="col-md-6 col-sm-12 col-xs-12">
-                  <img src="{{asset('/pays.png')}}" style=" width:100%">
+                  <video controls="" width="100%" height="auto" playsinline="">
+                     <source src="https://mazeoptions.com/Mazeoptions.mp4" type="video/mp4">
+                     Your browser does not support the video tag.
+                   </video>
                  </div>
             </div>
         </div>
