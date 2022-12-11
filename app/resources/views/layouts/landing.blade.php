0b24cb2152e3824933400516a2c3d5c1964ba23f
Michael Kachi <jesmikky@gmail.com>
2022-12-11 17:07:03 +0100

    fav added

--- app/resources/views/layouts/landing.blade.php
+++ app/resources/views/layouts/landing.blade.php
@@ -90,17 +90,17 @@
       <script src="{{asset('/frontend_assets/js/custom.js')}}"></script>
 
 <!--Start of Tawk.to Script-->
+<!-- Smartsupp Live Chat script -->
 <script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/61e72346b84f7301d32bb46b/1fpne3jo7';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
+  var _smartsupp = _smartsupp || {};
+  _smartsupp.key = '083302cfd8f6e17040e981b3e09637a72b5f551b';
+  window.smartsupp||(function(d) {
+    var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+    s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+    c.type='text/javascript';c.charset='utf-8';c.async=true;
+    c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+  })(document);
+  </script>
  
 <!--End of Tawk.to Script-->	</body>
 </html>
