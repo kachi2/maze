5f0e3bfbb77aef858085d0a0c197e7107c6fec74
Michael Ozoudeh <jesmikky@gmail.com>
2023-09-28 05:04:29 +0100

    agent user completed

--- app/routes/web.php
+++ app/routes/web.php
@@ -171,7 +171,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-        Route::group(['prefix' => 'affiliates', 'as' => 'affiliates.'], function(){
+        Route::domain('affiliates.mazeoptions.com')->group(['prefix' => 'affiliates', 'as' => 'affiliates.'], function(){
             Route::get('/register', 'Agency\AuthController@register')->name('register');
             Route::post('/register/submit', 'Agency\AuthController@registers')->name('registers');
             Route::get('/registration/{id}', 'Agency\AuthController@CompleteRegistration')->name('registration');
