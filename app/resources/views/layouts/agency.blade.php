fa908240e7e3a3456bbc1e589367aa8d855cf4fa
Michael Ozoudeh <jesmikky@gmail.com>
2023-09-28 05:06:54 +0100

    agent user completed

--- app/resources/views/layouts/agency.blade.php
+++ app/resources/views/layouts/agency.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8" />
         <title>Mazeoptions Affiliates</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="Mazeoptions Affiliates, Earn big when you refer your friends and family to mazeoptions" name="" />
         <meta content="" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <!-- App favicon -->
